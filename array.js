795e53a2a1a8bddff9f33d57d5e78710d153ea1e
Tarunika B <tarunikabaktha@gmail.com>
2025-12-05 08:43:05 +0530

    Day 4

---
+++ array.js
@@ -0,0 +1,33 @@
+let arr=["Arun","varun","Tarun"];
+
+console.log("Unshift");
+arr.unshift("Karun");
+console.log(arr);
+
+console.log("push");
+arr.push("Zera");
+console.log(arr);
+
+console.log("pop");
+arr.pop();
+console.log(arr);
+
+console.log("Shift");
+arr.shift();
+console.log(arr);
+
+console.log("Splice");
+arr.splice(1,1,"Ravi");
+console.log(arr);
+
+console.log("slice");
+arr.splice(1,2);
+console.log(arr);
+
+console.log("Unshift");
+arr.unshift("Viks","Tharunika");
+console.log(arr);
+
+console.log("Slice");
+let newArr=arr.slice(0,2);
+console.log(newArr);
